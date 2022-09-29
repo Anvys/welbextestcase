8399e367b1df88baa086e80b4bc0173abb4154b1
Anvys <houk05@yandex.ru>
2022-09-29 16:48:59 +0300

    build

--- src/components/Paginator/Paginator.tsx
+++ src/components/Paginator/Paginator.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import s from './Paginator.module.scss'
 import {useSelector} from "react-redux";
 import {RadarSelectors} from "../../redux/Selectors";
@@ -8,7 +8,8 @@ import {RadarSlice, RadarThunks} from "../../redux/reducers/radarSlice";
 type TProps = {}
 export const Paginator: React.FC<TProps> = (props) => {
     const dispatch = useAppDispatch()
-const filter = useSelector(RadarSelectors.getFilter)
+    const [showCount, setShowCount] = useState(0)
+    const filter = useSelector(RadarSelectors.getFilter)
     const page = useSelector(RadarSelectors.getPage)
     const pageCount = useSelector(RadarSelectors.getCount)
     const total = useSelector(RadarSelectors.getTotal)
@@ -36,8 +37,10 @@ const filter = useSelector(RadarSelectors.getFilter)
     return (
         <div className={s.paginator}>
             <div className={s.main}>
-                {new Array(pages).fill(1).map((v, i) =>
-                    <div key={i} onClick={onPageClick(i+1)} className={page === i + 1 ? s.selected : ''}>{i + 1}</div>)}
+                {<button disabled={!(showCount>0)} onClick={()=>setShowCount(a=>a-1)}>prev</button>}
+                {new Array(pages).fill(1).map((v, i) =>(i>=showCount*10 && i<10*showCount+10)?
+                    <div key={i} onClick={onPageClick(i+1)} className={page === i + 1 ? s.selected : ''}>{i + 1}</div>:null)}
+                {<button onClick={()=>setShowCount(a=>a+1)} disabled={!(showCount*10+10<pages)}>next</button>}
             </div>
             <div className={s.pageCount}>
                 <button className={pageCount === 10 ? s.selected:''} onClick={onPageCountClick(10)} type={'button'}>10</button>
